5480bf40135011580565a66ed60f70fea808c611
Sanjeev Kaushik <sanjeevkaushik90684@gmail.com>
2026-01-22 11:42:11 +0530

    responsive navbar done

--- vite-project/src/component/Home/navbar.jsx
+++ vite-project/src/component/Home/navbar.jsx
@@ -1,28 +1,28 @@
+import { useState } from "react"
 import { IoHome } from "react-icons/io5";
-import { FaMobileAlt } from "react-icons/fa";
-import { FaTv } from "react-icons/fa";
-import { FaLaptopCode } from "react-icons/fa";
+import { FaMobileAlt, FaLaptopCode, FaTv } from "react-icons/fa";
 import { FcAbout } from "react-icons/fc";
 import { FaMicroblog } from "react-icons/fa6";
 import { IoIosContact } from "react-icons/io";
+import { FaBars } from "react-icons/fa";
+
 
 
 
 export default function Navbar() {
+
     const navMenu = [
         { name: 'Home', icon: <IoHome />, link: '/' },
         { name: 'Mobile', icon: <FaMobileAlt />, link: '/mobile' },
         { name: 'Laptop', icon: <FaLaptopCode />, link: '/laptop' },
         { name: 'TV', icon: <FaTv />, link: '/tv' },
-        { name: 'About', icon: <FcAbout />, link: '/about' },
-        { name: 'Blog', icon: <FaMicroblog />, link: '/blog' },
-        { name: 'Contact us', icon: <IoIosContact />, link: '/contact-us' },
-
 
     ]
+
     return (
         <header className="px-8 py-4 ">
             <nav className="flex items-center justify-between bg-blue-700 py-5 px-10 rounded-3xl shadow-2xl shadow-black ">
+
 
                 <div>
 
@@ -30,12 +30,14 @@ export default function Navbar() {
 
                 </div>
 
-                <ul className="flex items-center gap-5 text-white">
+
+
+                <ul className="hidden lg:flex items-center gap-5 text-white">
 
                     {
-                        navMenu.map(({ icon, name, link }, index) => (
-                            <a href="{link}">
-                                <li className="flex items-center gap-2 hover:bg-orange-400 p-2 rounded-lg">
+                        navMenu.map(({ icon, name, link }, key) => (
+                            <a key={key} href={link}>
+                                <li className="flex items-center gap-2 hover:bg-orange-400 p-2 text-lg rounded-lg duration-500 ">
                                     {icon}
                                     <h3>{name}</h3>
                                 </li>
@@ -46,9 +48,13 @@ export default function Navbar() {
 
                 </ul>
 
-                <div className="flex items-center gap-3">
+                <div className="hidden lg:flex items-center gap-3 text-lg">
                     <button className="bg-amber-500 rounded-2xl text-white p-2">Sign in</button>
                     <button className="bg-gray-700 text-white rounded-2xl  p-2">Sign up</button>
+                </div>
+
+                <div className="flex lg:hidden">
+                    <FaBars />
                 </div>
             </nav>
         </header>
